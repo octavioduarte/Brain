2e7b3ad4fd04e6daa45b4ffbc47e45f30310edfa
octavioduarte <octavioduarte2105@gmail.com>
2023-11-25 20:37:30 -0300

    feat: add attributes name - active - created_at and updated_at on producer model

--- src/domain/models/producer.ts
+++ src/domain/models/producer.ts
@@ -2,11 +2,18 @@ import { FarmDB, FarmModel } from "./farm";
 
 export type ProducerDB = {
   id: number;
+  active: boolean;
+  name: string;
   document: string;
   document_type: string;
   farm: FarmDB[];
+  created_at: Date;
+  updated_at: Date;
 };
 
-export type ProducerModel = Omit<ProducerDB, "id" | 'farm'> & {
-    farm: FarmModel
+export type ProducerModel = Omit<
+  ProducerDB,
+  "id" | "farm" | "active" | "created_at" | "updated_at"
+> & {
+  farm: FarmModel;
 };
